b60f71833b8189200c361b757fca93da92552336
Sabrina Li <sabrinali0312@gmail.com>
2019-07-10 20:39:12 -0400

    remove input form for now

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,6 @@ dotenv.config();
 function App() {  
   return (
     <div className="App" style={{ height: '100vh', width: '100%' }}>
-      {/* <SimpleMapPage></SimpleMapPage> */}
-      {/* <MyMapComponent 
-        isMarkerShown 
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}/> */}
         {/* <DirectionsForm /> */}
         <DemoApp />
     </div>
