7efb937d7c2e9ec7791b72b53be76e4007f0870d
Sabrina Li <sabrinali0312@gmail.com>
2019-07-16 15:44:19 -0400

    add animation for welcome page

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -11,12 +11,16 @@ const Welcome = (props)=>{
                 <Typography variant="h6" gutterBottom className="fixed-title"> Know Your Roads</Typography>
             </header>
             <main>
-                <Typography variant="h4" gutterBottom className="facts">Nearly 1.25 million people die in road crashes each year</Typography>
-                <Typography variant="h4" gutterBottom className="facts">An additional 20-50 million are injured or disabled each year</Typography>
-                <Typography variant="h4" gutterBottom className="facts">More than 90 people die in car accidents every day</Typography>
-                <Typography variant="h4" gutterBottom className="facts">Every 16 minutes, a car accident occurs that results in death</Typography>
-                <Typography variant="h4" gutterBottom className="facts">Drive safe, know your roads!</Typography>
+                <Typography variant="h4" gutterBottom className="facts">Nearly <span className="red">1.25 million</span> people die in road crashes each year</Typography>
+                <Typography variant="h4" gutterBottom className="facts">An additional <span className="red">20-50 million</span> are injured or disabled each year</Typography>
+                <Typography variant="h4" gutterBottom className="facts">More than <span className="red">90</span> people die in car accidents every day</Typography>
+                <Typography variant="h4" gutterBottom className="facts">Every <span className="red">16 minutes</span>, a car accident occurs that results in death</Typography>
+                <Typography variant="h4" gutterBottom className="facts">Drive safe, <span className="red">K</span>now <span className="red">Y</span>our <span className="red">R</span>oads!</Typography>
             </main>
+            <a className="footer" href="/map">
+                <Typography variant="body1" gutterBottom className="page-text">GO TO MAP</Typography>
+                <i class="fas fa-chevron-down fas-4x"></i>
+            </a>
         </div>
     </>
 }
