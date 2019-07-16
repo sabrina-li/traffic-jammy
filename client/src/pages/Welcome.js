8862460d4c98d3a11ad79746d20a59c8988e3039
Sabrina Li <sabrinali0312@gmail.com>
2019-07-16 14:56:55 -0400

    adding facts

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -7,10 +7,16 @@ const Welcome = (props)=>{
     return <>
         <div id="background">
             <header>
-            <Typography variant="h3" gutterBottom className="fixed-title">KYR </Typography>
-            <Typography variant="h4" gutterBottom className="fixed-title"> Know Your Roads</Typography>
+                <Typography variant="h2" gutterBottom className="fixed-title">KYR </Typography>
+                <Typography variant="h6" gutterBottom className="fixed-title"> Know Your Roads</Typography>
             </header>
-            
+            <main>
+                <Typography variant="h4" gutterBottom className="facts">Nearly 1.25 million people die in road crashes each year</Typography>
+                <Typography variant="h4" gutterBottom className="facts">An additional 20-50 million are injured or disabled each year</Typography>
+                <Typography variant="h4" gutterBottom className="facts">More than 90 people die in car accidents every day</Typography>
+                <Typography variant="h4" gutterBottom className="facts">Every 16 minutes, a car accident occurs that results in death</Typography>
+                <Typography variant="h4" gutterBottom className="facts">Drive safe, know your roads!</Typography>
+            </main>
         </div>
     </>
 }
